7b2a2c43cfdcf148e9b1978b1536f3a9157ab4c5
xiongzhenglong <xiongzl@1391.com>
2017-10-16 22:36:05 +0800

    Crawer

---
+++ GetComics/Crawer/Jobs/QQ_Comic_Job.cs
@@ -0,0 +1,31 @@
+﻿using Chloe;
+using Chloe.SqlServer;
+using Framework.Common.Extension;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawer.Jobs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DisallowConcurrentExecution]
+    public class QQ_Comic_Job:IJob
+    {
+        MsSqlContext dbcontext;
+        public QQ_Comic_Job()
+        {
+            dbcontext = new MsSqlContext("Mssql".ValueOfAppSetting());
+
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            IQuery<User> q = context.Query<User>();
+        }
+    }
+}
